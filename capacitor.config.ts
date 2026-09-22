ce76ccc69b696f4348d5deea08f3873d0b23d9e1
PapConAbrilar <benjapinto.bp@gmail.com>
2026-09-22 13:29:19 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NamNam',
+  webDir: 'www'
+};
+
+export default config;
